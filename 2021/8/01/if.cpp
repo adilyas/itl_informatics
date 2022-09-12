6bdb6da0a1db8b012eda62ef0c968c6bc3d4a970
Ilyas Adygamov <adygamovilyas@gmail.com>
2022-09-12 15:23:18 +0300

    add snippets for 8th grade

--- 2021/8/01/if.cpp
+++ 2021/8/01/if.cpp
@@ -14,13 +14,25 @@ int main() {
         cout << "negative\n";
     }
 
-    if (a >= 10000000) {
+    if (a >= 10000000 || a <= -10000000) {
         cout << "sorry, too big\n";
+    }
+
+    if (a <= 10000000 && a >= -10000000) {
+        cout << "ok\n";
     }
 
     cout << a << endl;
 
     if (a = 5) { // так нельзя
+        cout << "a = 5 is ok\n";
+    }
+    if (a = 0) {
+        cout << "a = 0 is not ok\n";
+    }
+    a = 0;
+    if (a) {
+        cout << "this has the same effect as the previous if\n";
     }
 
     cout << a << endl;
