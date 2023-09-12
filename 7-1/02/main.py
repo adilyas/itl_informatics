21e99ce9494d7f67509bb19c696b5f0057f10153
Provet <>
2023-09-12 18:22:49 +0300

    more examples

---
+++ 7-1/02/main.py
@@ -0,0 +1,21 @@
+a = 5 
+b = 2
+
+print(a % b)
+
+s = input()
+print("2 * s = " + 2 * s)
+
+a = int(input())
+b = int(input())
+
+print("a + b = " + str(a + b))
+
+c = 7.3297
+print(int(c))
+print("%.3f \n %.8f" % (c, c))
+print("Date %02d:%02d:%02d" % (6, 4, 30))
+
+name = input()
+print("Hello %s!" % name)
+
